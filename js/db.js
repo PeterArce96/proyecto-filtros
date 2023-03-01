5129b1763078f775e27318a192652e56444220be
PeterArce96 <parce0496@gmail.com>
2023-02-28 19:50:24 -0500

    filtros - agregando zapatillas Adidas, pumas a db.js

---
+++ js/db.js
@@ -0,0 +1,302 @@
+const autos = [
+	{
+		marca: 'Adidas',
+		color: 'Negro',
+		talla: 40,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Blanco',
+		talla: 40,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Rojo',
+		talla: 40,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Negro',
+		talla: 41,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Blanco',
+		talla: 41,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Rojo',
+		talla: 41,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Negro',
+		talla: 42,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Blanco',
+		talla: 42,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Rojo',
+		talla: 42,
+		precio: 249,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Negro',
+		talla: 35,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Blanco',
+		talla: 35,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Rojo',
+		talla: 35,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Negro',
+		talla: 36,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Blanco',
+		talla: 36,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Rojo',
+		talla: 36,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Negro',
+		talla: 37,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Blanco',
+		talla: 37,
+		precio: 199,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Adidas',
+		color: 'Rojo',
+		talla: 37,
+		precio: 199,
+		genero: 'femenino'
+	},
+
+    
+    {
+		marca: 'Puma',
+		color: 'Negro',
+		talla: 35,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Blanco',
+		talla: 35,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Rojo',
+		talla: 35,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Plomo',
+		talla: 35,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Negro',
+		talla: 36,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Blanco',
+		talla: 36,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Rojo',
+		talla: 36,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Plomo',
+		talla: 36,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Negro',
+		talla: 37,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Blanco',
+		talla: 37,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Rojo',
+		talla: 37,
+		precio: 179,
+		genero: 'femenino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Plomo',
+		talla: 37,
+		precio: 179,
+		genero: 'femenino'
+	},
+
+    {
+		marca: 'Puma',
+		color: 'Negro',
+		talla: 39,
+		precio: 189,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Blanco',
+		talla: 39,
+		precio: 189,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Rojo',
+		talla: 39,
+		precio: 189,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Plomo',
+		talla: 39,
+		precio: 189,
+		genero: 'masculino'
+	},
+
+    {
+		marca: 'Puma',
+		color: 'Negro',
+		talla: 42,
+		precio: 229,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Blanco',
+		talla: 42,
+		precio: 229,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Rojo',
+		talla: 42,
+		precio: 229,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Plomo',
+		talla: 42,
+		precio: 229,
+		genero: 'masculino'
+	},
+    
+    {
+		marca: 'Puma',
+		color: 'Negro',
+		talla: 44,
+		precio: 239,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Blanco',
+		talla: 44,
+		precio: 239,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Rojo',
+		talla: 44,
+		precio: 239,
+		genero: 'masculino'
+	},
+    {
+		marca: 'Puma',
+		color: 'Plomo',
+		talla: 44,
+		precio: 239,
+		genero: 'masculino'
+	},
+
+];
